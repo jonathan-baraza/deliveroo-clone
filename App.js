b0937dbbbd8adffc68cce898d7ab9e38e01925a9
jonathan_baraza <jbaraza46@gmail.com>
2023-05-11 16:07:08 +0300

    setup react navigation

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
-
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text className="">Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
