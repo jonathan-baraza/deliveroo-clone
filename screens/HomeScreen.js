e46e7cbfa13ec1113c84e4891e4600628fdbbc9b
jonathan_baraza <jbaraza46@gmail.com>
2023-05-11 16:34:30 +0300

    home screen initialization

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text className="">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      {/* Header */}
+
+      <View className="flex-row pb-3 mx-4 items-center space-x-2 pt-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View>
+          <Text className="font-sm text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">Current Location</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
